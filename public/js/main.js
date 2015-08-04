6514a41899e1a99f5d32a9c7fb24f537f5ec4bde
Timon <timonpike@gmail.com>
2015-08-04 12:04:45 -0700

    Made link generation more usable and responsive

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,49 @@ var urlCopy = $('#generated-url');
 var copyMessage = $('#copy-message');
 var titleElem = $('#title');
 var urlElem = $('#url');
+var urlSave = '';
 
 copyMessage.hide();
 
-urlElem.on('change', function(e) {
+urlElem.on('focus', function(e) {
+    var url = urlElem.val();
+
+    if(url === '') {
+        urlElem.val('http://');
+    }
+});
+
+urlElem.on('blur', function(e) {
+    var url = urlElem.val();
+    console.log(url);
+    if(url === 'http://') {
+        urlElem.val('');
+    }
+});
+
+urlElem.on('paste', function(e) {
+    var url = urlElem.val();
+    console.log(url)
+    if(url === 'http://') {
+        urlElem.val('').val(e.target.value);
+    }
+})
+
+// titleElem.on('change', function(e) {
+//     var url = encodeURIComponent(urlElem.val());
+//     var title = encodeURIComponent(titleElem.val());
+
+//     if(title !== '' && url !== '') {
+//         urlCopy.text(HOSTNAME + url + '&t=' + title)
+//         copyMessage.show();
+//     } else {
+//         urlCopy.text('');
+//         copyMessage.hide();
+//     }
+// });
+
+$('#gen-url').submit(function(e) {
+    e.preventDefault();
     var url = encodeURIComponent(urlElem.val());
     var title = encodeURIComponent(titleElem.val());
 
@@ -16,23 +55,30 @@ urlElem.on('change', function(e) {
     }
 
     if(url !== '') {
-        urlCopy.text(HOSTNAME + url + title);
+        urlCopy.val(HOSTNAME + url + title);
+        urlSave = HOSTNAME + url + title;
         copyMessage.show();
     } else {
-        urlCopy.text('');
+        urlCopy.val('');
+        urlSave = '';
         copyMessage.hide();
     }
 });
 
-titleElem.on('change', function(e) {
-    var url = encodeURIComponent(urlElem.val());
-    var title = encodeURIComponent(titleElem.val());
+urlCopy.click(function(e) {
+    this.select();
+});
 
-    if(title !== '' && url !== '') {
-        urlCopy.text(HOSTNAME + url + '&t=' + title)
-        copyMessage.show();
+urlCopy.blur(function(e) {
+    urlCopy.val(urlSave);
+});
+
+urlCopy.on('keydown', function(e) {
+    urlSave = urlCopy.val();
+    if(e.crtlKey || !e.metaKey) {
+        // console.log()
     } else {
-        urlCopy.text('');
-        copyMessage.hide();
+        e.preventDefault();
+        console.log("prevented")
     }
 });
